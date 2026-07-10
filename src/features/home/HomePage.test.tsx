075f7492f02b2b08dc3e2868ab91bf03f64b4f24
zehro <zehrofresher@gmail.com>
2026-07-10 05:01:32 -0400

    Fixing property 'toBeInTheDocument' does not exist

--- src/features/home/HomePage.test.tsx
+++ src/features/home/HomePage.test.tsx
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom/vitest";
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { HomePage } from "./HomePage";
